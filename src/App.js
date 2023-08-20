ffba133220ebf6a04914933735963ac9df43b856
Jesus Moreno <moreno_2318@yahoo.com>
2023-08-19 18:54:27 -0700

    React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PostCreate from './PostCreate'
+import Postlist from './PostList'
+
+const App = () => {
+    return (
+        <div className='container'>
+            <h1>Create Post</h1>
+            <PostCreate />
+            <hr />
+            <h1>Posts</h1>
+            <Postlist />
+        </div>
+    )
+}
+
+export default App
